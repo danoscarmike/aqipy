37b7929b261ad0d26f3ad40d1295ae6b0d8b2145
danoscarmike <omeara.dan@gmail.com>
2018-11-20 17:57:45 -0800

    fix setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='aqipy',
-    version='0.2.0',
+    version='0.2.1',
     author='Dan O\'Meara',
     author_email='omeara.dan@gmail.com',
     description='A simple CLI to get live Air Quality Indices',
