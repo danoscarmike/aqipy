40b49ad98dd952f5caf5b5b2179e73f3596f2f3e
danoscarmike <omeara.dan@gmail.com>
2018-11-20 17:53:50 -0800

    prepare package for PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='aqipy',
     version='0.2.0',
     author='Dan O\'Meara',
     author_email='omeara.dan@gmail.com',
+    description='A simple CLI to get live Air Quality Indices',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danoscarmike/aqipy",
     packages=['aqipy'],
     entry_points={
         'console_scripts': [
@@ -15,6 +22,8 @@ setup(
         'requests',
     ],
     classifiers=[
+        'Programming Language :: Python :: 3',
         'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
     ]
 )
