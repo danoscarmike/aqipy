306628ed820b60e51459d4cd47df142d15efff27
Dan O'Meara <omeara.dan@gmail.com>
2020-08-24 13:36:58 -0700

    chore: blackened

--- setup.py
+++ setup.py
@@ -4,26 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='aqipy',
-    version='0.3.0',
-    author='Dan O\'Meara',
-    author_email='omeara.dan@gmail.com',
-    description='A simple CLI to get live Air Quality Indices',
+    name="aqipy",
+    version="0.3.0",
+    author="Dan O'Meara",
+    author_email="omeara.dan@gmail.com",
+    description="A simple CLI to get live Air Quality Indices",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/danoscarmike/aqipy",
-    packages=['aqipy'],
-    entry_points={
-        'console_scripts': [
-            'aqipy = aqipy.__main__:main'
-        ]},
-    install_requires=[
-        'click',
-        'requests',
-    ],
+    packages=["aqipy"],
+    entry_points={"console_scripts": ["aqipy = aqipy.__main__:main"]},
+    install_requires=["click", "requests",],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-    ]
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
